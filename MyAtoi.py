110d4a9f03c75aafeefc2e505d7a71cd900f2a49
linhaoxiang <linhaoxiang32@gmail.com>
2015-04-21 23:38:57 +0800

    4.21 add 3problem

---
+++ MyAtoi.py
@@ -0,0 +1,9 @@
+__author__ = 'lenovo'
+# -*- coding: utf-8 -*-
+class Solution:
+    # @param {string} str
+    # @return {integer}
+    def myAtoi(self, str):
+        lens = len(str)
+        for i in range(0,lens):
+            return 0
